34383ad6aa71ed51b0d921d9eb33c849c13b5910
codemorra <codemorra@posteo.de>
2026-08-14 15:51:04 +0200

    feat: add initial application routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,19 @@
-import "./App.css";
+import { Route, Routes } from 'react-router';
+import Home from './pages/Home.tsx';
+import Gallery from './pages/Gallery.tsx';
+import Login from './pages/Login.tsx';
+import Register from './pages/Register.tsx';
+import './App.css';
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/gallery" element={<Gallery />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
 }
 
 export default App;
