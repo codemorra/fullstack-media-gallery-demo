fa31f40d1b87379c79769ab86db72a65b71bc938
codemorra <codemorra@posteo.de>
2026-08-14 14:57:56 +0200

    chore: configure Prettier

---
+++ frontend/prettier.config.ts
@@ -0,0 +1,11 @@
+import { type Config } from "prettier";
+
+const config: Config = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: "all",
+  tabWidth: 2,
+  bracketSpacing: true,
+};
+
+export default config;
